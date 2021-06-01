d97b165994d15a7086ef3334a04f1a839a4a51e6
emmepi92 <mariapiacoccioli@live.it>
2021-06-01 13:47:06 +0200

    finished lol

---
+++ js/script.js
@@ -0,0 +1,13 @@
+for ( x = 1; x <= 100; x ++ ) {
+    
+    if ( x % 3 === 0 && x % 5 === 0) {
+        document.getElementById("result").innerHTML += "Fizz & Buzz <br/>";
+    }
+    else if ( x % 3 === 0) {
+        document.getElementById("result").innerHTML += "Fizz <br/>";
+    } else if ( x % 5 === 0) {
+        document.getElementById("result").innerHTML += "Buzz <br/>";
+    } else {
+        document.getElementById("result").innerHTML += x + " <br/>";
+    }
+}
