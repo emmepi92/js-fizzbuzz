dcfa8c123eea8414e3075ba2664781380a05e4f1
emmepi92 <mariapiacoccioli@live.it>
2021-06-01 15:59:32 +0200

    better

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 var outputHtml = document.getElementById("result");    
+var result;     
      
 for ( x = 1; x <= 100; x ++ ) {       
     if ( x % 3 === 0 && x % 5 === 0) {          
-        outputHtml.innerHTML += "FizzBuzz <br/>";        
+        result = "FizzBuzz <br/>";        
     } else if ( x % 3 === 0) {      
-        outputHtml.innerHTML += "Fizz <br/>";        
+        result = "Fizz <br/>";        
     } else if ( x % 5 === 0) {      
-        outputHtml.innerHTML += "Buzz <br/>";        
+        result = "Buzz <br/>";        
     } else {        
-        outputHtml.innerHTML += x + " <br/>";        
-    }       
+        result = x + " <br/>";        
+    } 
+    outputHtml.innerHTML += result;      
 }       
