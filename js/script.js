1e18ce569b39cc3bd40b79e661013a0d2d88cbe5
emmepi92 <mariapiacoccioli@live.it>
2021-06-01 14:04:19 +0200

    detail

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-for ( x = 1; x <= 100; x ++ ) {    
-    if ( x % 3 === 0 && x % 5 === 0) {
-        document.getElementById("result").innerHTML += "Fizz & Buzz <br/>";
-    } else if ( x % 3 === 0) {
-        document.getElementById("result").innerHTML += "Fizz <br/>";
-    } else if ( x % 5 === 0) {
-        document.getElementById("result").innerHTML += "Buzz <br/>";
-    } else {
-        document.getElementById("result").innerHTML += x + " <br/>";
-    }
-}
+for ( x = 0; x < 100; x ++ ) {       
+    if ( x % 3 === 0 && x % 5 === 0) {          
+        document.getElementById("result").innerHTML += "FizzBuzz <br/>";        
+    } else if ( x % 3 === 0) {      
+        document.getElementById("result").innerHTML += "Fizz <br/>";        
+    } else if ( x % 5 === 0) {      
+        document.getElementById("result").innerHTML += "Buzz <br/>";        
+    } else {        
+        document.getElementById("result").innerHTML += x + " <br/>";        
+    }       
+}       
